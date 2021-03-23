8b261e86b3626f3eec6609bc25ad4f95afeb069d
mireiahernandez <mireiahernandezcaralt@outlook.es>
2021-03-23 15:13:23 -0400

    initial commit

--- network/mlp.py
+++ network/mlp.py
@@ -1,22 +1,23 @@
-import torch
+import torch as th
 import torch.nn as nn
 
-class MLP(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
         super(MLP, self).__init__()
         self.input_size = input_size
-        self.hidden_size  = hidden_size
+        self.hidden_size1  = hidden_size1
+        self.hidden_size2 = hidden_size2
         self.output_size = output_size
-        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
-        nn.init.normal_(self.fc1.weight, mean=0.0, std=0.01) 
-        self.relu = torch.nn.ReLU()
-        self.fc2 = torch.nn.Linear(self.hidden_size, self.output_size)
-        nn.init.normal_(self.fc2.weight, mean=0.0, std=0.01) 
-        self.sigmoid = torch.nn.Sigmoid()
+
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(self.input_size, self.hidden_size1)
+        self.fc2 = nn.Linear(self.hidden_size1, self.hidden_size2)
+        self.fc3 = nn.Linear(self.hidden_size2, self.output_size)
+
 
     def forward(self, x):
-        hidden = self.fc1(x)
-        relu = self.relu(hidden)
-        output = self.fc2(relu)
-        output = self.sigmoid(output)
-        return output
+        hidden1 = self.relu(self.fc1(x))
+        hidden2 = self.relu(self.fc2(hidden1))
+        output = self.fc3(hidden2)
+        output_clipped = th.max(th.min(output,  th.ones(size=output.shape)), th.zeros(size=output.shape))
+        return output_clipped
