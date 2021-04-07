85f8f459cd897052f0639a42411bb4d780d0deb3
Joanna Materzynska <joanna@Joannas-MacBook-Pro.local>
2021-04-07 12:40:01 +0200

    shuffle training data

--- toy_train.py
+++ toy_train.py
@@ -31,7 +31,7 @@ class TimesDataset(Dataset):
         return len(self.times)
 
     def __getitem__(self, idx):
-        return self.times[idx]
+        return self.times[idx], idx
 
 
 
@@ -47,9 +47,9 @@ def get_plot(input_times, output_times, gt_times):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--movie', type=str, help='movie name')
-    parser.add_argument('--direction', type=str, help='m2b or b2m')
-    parser.add_argument('--loss_type', type=str, help='GT or R')
+    parser.add_argument('--movie', default='Harry.Potter.and.the.Sorcerers.Stone', type=str, help='movie name')
+    parser.add_argument('--direction', default='m2b', type=str, help='m2b or b2m')
+    parser.add_argument('--loss_type', default='GT', type=str, help='GT or R')
     parser.add_argument('--try_num', type=str, help='try number')
     parser.add_argument('--kernel_type', type=str, help='kernel type')
     args = parser.parse_args()
@@ -68,7 +68,8 @@ if __name__ == "__main__":
     kernel_type = args.kernel_type
 
     # Tensorboard summary writer
-    exp_name = f"train_{direction}_kernel_{kernel_type}_loss_{loss_type}_noSigmoid_weightdecay_{weight_decay}_h1_{hidden_size1}_h2_{hidden_size2}_lr_{lr}_batchsize_{batch_size}_try_{args.try_num}"
+    exp_name = f"train_{direction}_kernel_{kernel_type}_loss_{loss_type}_SHUFFLE_try_{args.try_num}"
+    # exp_name = f"train_{direction}_kernel_{kernel_type}_loss_{loss_type}_noSigmoid_weightdecay_{weight_decay}_h1_{hidden_size1}_h2_{hidden_size2}_lr_{lr}_batchsize_{batch_size}_try_{args.try_num}"
     writer = SummaryWriter(log_dir="runs/" + exp_name)
 
     # Get feats
@@ -93,12 +94,12 @@ if __name__ == "__main__":
         input_feats = image_feats
         len_input = len_image
         output_feats = text_feats
-        len_output  = len_text
+        len_output = len_text
     else: 
         input_feats = text_feats
         len_input = len_text
         output_feats = image_feats
-        len_output  = len_image
+        len_output = len_image
 
     # Get input and output times
     input_times = th.FloatTensor(np.arange(len_input)) 
@@ -117,12 +118,13 @@ if __name__ == "__main__":
     # Get toy input feats: output_feats -> toy_input_feats
     # we need the reverse mapping input -> output
     toy_input_feats = reverse_mapping(output_feats, f_times, kernel_type)
-    
-   
+
+    # from toy_input_feats to gt_output_feats (input -> output)
+    gt_output_feats = reverse_mapping(toy_input_feats, invf_times, kernel_type)
 
     # Create times dataset and dataloader
     dataset = TimesDataset(output_times_scaled)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, sampler=None,
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, sampler=None,
                 batch_sampler=None, num_workers=1, collate_fn=None)
     
     # Define model
@@ -144,13 +146,18 @@ if __name__ == "__main__":
     epoch = 0
     while abs(loss_prev - loss_now) > 1e-10:
         pred_invf_times_scaled = []
+        index = []
         # Run times through alignment network (mlp)
         for i, batch in enumerate(tqdm(dataloader)):
+            batch, idx = batch
             invf_output = model.forward(batch.unsqueeze(1))
-            pred_invf_times_scaled.append(invf_output) 
+            pred_invf_times_scaled.append(invf_output)
+            index.append(idx)
 
         # outputs are between 0 and 1 and have shape (No)
         pred_invf_times_scaled = th.cat(pred_invf_times_scaled, dim=0).squeeze(1) # shape No
+        index = th.cat(index, dim=0)
+        pred_invf_times_scaled = pred_invf_times_scaled[index]
         
         # re-scale to 0 len_output -1
         pred_invf_times = pred_invf_times_scaled * (len_input - 1) # shape No
